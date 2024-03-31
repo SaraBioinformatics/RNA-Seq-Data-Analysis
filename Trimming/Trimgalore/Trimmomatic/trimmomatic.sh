11ee59d92996ea5c8bef61be13c4389476679e63
Sarita Maurya <115336007+Saritamaurya1234@users.noreply.github.com>
2024-03-31 16:51:24 +0530

    Create trimmomatic.sh

---
+++ Trimming/Trimgalore/Trimmomatic/trimmomatic.sh
@@ -0,0 +1,12 @@
+#######open terminal
+cammand line-
+java -jar trimmomatic-0.39.jar SE -phred33 SRR5896247.fastq.gz output47.fq.gz ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36 
+java -jar trimmomatic-0.39.jar SE -phred33 SRR5896248.fastq.gz output48.fq.gz ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+
+ ###paired end reads
+ java -jar trimmomatic-0.39.jar PE input_forward.fq.gz input_reverse.fq.gz output_forward_paired.fq.gz output_forward_unpaired.fq.gz output_reverse_paired.fq.gz output_reverse_unpaired.fq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2:True LEADING:3 TRAILING:3 MINLEN:36
+
+####for single end reads###
+
+java -jar trimmomatic-0.35.jar SE -phred33 input.fq.gz output.fq.gz ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+
